cc21c86a31b06061d1db8613558aef0e363160b3
Joao Pedro Dias <jpdias@live.com.pt>
2015-05-12 21:29:23 +0100

    Added User class (Warehouse/Store Users)

---
+++ EnterpriseDistributedApplication/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace EnterpriseDistributedApplication
+{
+    [DataContract]
+    class User
+    {
+        public BsonObjectId _id;
+        [DataMember]
+        public string Username { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+
+        public User(string user, string pass)
+        {
+            Username = user;
+            Password = pass;
+        }
+        
+    }
+}
