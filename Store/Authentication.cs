93e7b252982fe6262cd7d1bf397cad6b90dc5e42
Joao Pedro Dias <jpdias@live.com.pt>
2015-05-14 16:17:25 +0100

    Added missing files

---
+++ Store/Authentication.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using EnterpriseDistributedApplication;
+using MongoDB.Driver;
+
+namespace Store
+{
+    public class Authentication
+    {
+        private MongoConnectionHandler dbConnection;
+
+        public Authentication()
+        {
+            dbConnection = new MongoConnectionHandler("store", "admin", "eda_store");
+        }
+
+        public bool AuthenticateUser(User user)
+        {
+            var collection = dbConnection.dbClient.GetCollection<User>("users");
+
+            Debug.WriteLine(user.Username + "  " + user.Password);
+            var task = collection.Find(u => u.Username == user.Username).FirstOrDefaultAsync();
+
+            task.Wait();
+            var results = task.Result;
+            if (results.Password == user.Password)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool AuthenticateCustomer(Customer costumer)
+        {
+            var collection = dbConnection.dbClient.GetCollection<Customer>("customers");
+
+            var task = collection.Find(u => u.Email == costumer.Email).FirstOrDefaultAsync();
+
+            task.Wait();
+            var results = task.Result;
+            if (results.Password == costumer.Password)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool CreateCustomer(Customer costumer)
+        {
+            var collection = dbConnection.dbClient.GetCollection<Customer>("customers");
+
+            var task = collection.InsertOneAsync(costumer);
+
+            task.Wait();
+            var results = task;
+
+            if(results.IsCompleted)
+                return true;
+            
+            return false;
+        }
+    }
+}
---
+++ Warehouse/IWarehouseService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using EnterpriseDistributedApplication;
+
+namespace Warehouse
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWarehouseService" in both code and config file together.
+    [ServiceContract]
+    public interface IWarehouseService
+    {
+        [OperationContract(IsOneWay = true)]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void ReportToWarehouse(Book book);
+    }
+}
