5e76b9b752099396c69ec5b9c32b933bec28a64a
Joao Pedro Dias <jpdias@live.com.pt>
2015-05-17 23:43:13 +0100

    email.cs

---
+++ Store/Email.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using Mandrill;
+using Mandrill.Models;
+using Mandrill.Requests.Messages;
+
+namespace Store
+{
+    public static class Email
+    {
+        public static async void SendEmail(string destination, string subject, string content)
+        {
+            MandrillApi api = new MandrillApi("Kj-1SGPKFICoSgUIo9OEqw");
+
+            var mandrillRecipients = new List<EmailAddress>();
+            mandrillRecipients.Add(new EmailAddress(destination));
+            // add recipents to the mandrillRecipientsList
+            var email = new EmailMessage
+            {
+                To = mandrillRecipients,
+                FromEmail = "book@eda.com",
+                FromName = "Book Store Support",
+                Subject = subject,
+                Text = content,
+                RawMessage = content
+            };
+
+            await api.SendMessage(new SendMessageRequest(email));
+
+        }
+    }
+}
--- Store/StoreService.svc.cs
+++ Store/StoreService.svc.cs
@@ -16,8 +16,6 @@ using Store.WarehouseService;
 
 namespace Store
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "StoreService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select StoreService.svc or StoreService.svc.cs at the Solution Explorer and start debugging.
     public class StoreService : IStoreService
     {
         public List<Book> GetBooks()
